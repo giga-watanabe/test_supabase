73d4e0c798d78f08050233b0cab31e6c7ad3d81c
GIGA渡部 <yuki.watanabe@giga-system.co.jp>
2023-11-22 15:49:31 +0900

    restart 2回目

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+      baseUrl: process.env.BASE_URL || 'https://giga-watanabe.github.io/test_supabase/'
     }
   },
   ui: {
